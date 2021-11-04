66472d8c4b09890b79d01d56a419050da594dae0
Ludovic Minvielle <ludominvielle@gmail.com>
2021-11-04 11:05:42 +0100

    viewing yolo results

--- localization.py
+++ localization.py
@@ -1,3 +1,25 @@
-from yolov5 import train
+import cv2
+import glob
+from yolov5 import train, YOLOv5
 
-train.run(imgsz=256, batch_size=16, epochs=3, weights='yolov5s.pt', data='data.yaml')
+# train.run(imgsz=256, batch_size=16, epochs=300, weights='yolov5s.pt', data='data.yaml')
+
+path_weights = "runs/train/exp15/weights/best.pt"
+path_imgs = "../data/localization/test/"
+
+files_test = glob.glob(path_imgs + '*.png')
+
+yolo = YOLOv5(path_weights)
+results = yolo.predict(files_test)
+# predictions = results.pred[0]
+for im, predictions in zip(results.imgs, results.pred):
+    predictions = predictions.detach().to('cpu').numpy()
+
+    im_pred = im.copy()
+    for i in range(predictions.shape[0]):
+        print(predictions[i])
+        im_pred = cv2.rectangle(im_pred, tuple(predictions[i, [0, 2]].astype(int)), tuple(predictions[i, [1, 3]].astype(int)), (0, 255, 200), 1)
+    cv2.imshow('image', im)
+    cv2.imshow('image pred', im_pred)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows
